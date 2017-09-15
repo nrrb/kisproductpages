ac8b111f6b8925a6158603775031fd32cafb7904
Nick Bennett <nicholas.bennett@kellogg.northwestern.edu>
2017-09-15 16:01:39 -0500

    Remove static HTML files, replaced with generated files in the output/faculty subfolder. All faculty pages are now rendered using combination of JSON data and Jinja2 templates, using Python script to put them together.

--- compile_pages.py
+++ compile_pages.py
@@ -1,34 +1,41 @@
-from __future__ import print_function
 import json
-from jinja2 import Template
+import logging
+from jinja2 import Environment, FileSystemLoader
 
-for product_data_json in (	'data/products/faculty/T470s.json',
-							'data/products/faculty/X1Carbon5thGen.json',
-							'data/products/faculty/X1Yoga2ndGen.json',
-							'data/products/faculty/M910tDesktop.json'	):
+logging.basicConfig(level=logging.INFO)
 
-	print("Reading data from " + product_data_json + "...")
-	with open(product_data_json, 'rb') as f:
+def render_template_to_file(json_source_file):
+	logging.info("Reading data from " + json_source_file + "...")
+	with open(json_source_file, 'rb') as f:
 	    data = json.load(f)
 
 	audience = data['audience']
 
-	# Use the product data to render the appropriate product template
-
-	# From the data's template_type value, "laptop" or "desktop", use the appropriate product template file
+	# From the data's template_type value, "laptop", "desktop", or "index", use the appropriate product template file
 	if data['template_type'] == 'laptop':
-	    product_template_file = 'templates/' + audience + '/laptop.jinja2'
+	    template_file_path = 'templates/' + audience + '/laptop.jinja2'
 	elif data['template_type'] == 'desktop':
-	    product_template_file = 'templates/' + audience + '/desktop.jinja2'
+	    template_file_path = 'templates/' + audience + '/desktop.jinja2'
+	elif data['template_type'] == 'index':
+		template_file_path = 'templates/' + audience + '/index.jinja2'
 	else:
-	    print('The data file says the template_type is "' + data['template_type'] + '" but that\'s invalid. It should be "laptop" or "desktop".')
+	    logging.error('The data file says the template_type is "' + data['template_type'] + '" but that\'s invalid. It should be "laptop", "desktop", or "index".')
 	    exit(1)
 
-	print("Using template file " + product_template_file + ".")
-	with open(product_template_file, 'rb') as f:
-	    template = Template(f.read())
+	logging.info("Using template file " + template_file_path + ".")
+	with open(template_file_path, 'rb') as f:
+	    template = Environment(loader=FileSystemLoader('templates/' + audience)).from_string(f.read())
 
 	output_path = 'output/' + audience + '/' + data['output_filename']
+	logging.info("Outputting to " + output_path + ".")
 	with open(output_path, 'wb') as f:
 	    f.write(template.render(data))
-	print("Output to " + output_path + ".")
+
+
+if __name__=="__main__":
+	for json_source_file in (	'data/products/faculty/T470s.json',
+								'data/products/faculty/X1Carbon5thGen.json',
+								'data/products/faculty/X1Yoga2ndGen.json',
+								'data/products/faculty/M910tDesktop.json',
+								'data/products/faculty/index.json'	):
+		render_template_to_file(json_source_file)
